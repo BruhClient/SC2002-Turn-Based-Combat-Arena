fd222c6fdcfa88b4a22a4ff7a49cc268bfed6f48
ThomasChai288 <thomas.chai.qin.tze@gmail.com>
2026-03-20 16:57:08 +0800

    Action interface

--- BasicAttack.java
+++ BasicAttack.java
@@ -1,8 +1,8 @@
 public class BasicAttack implements Action {
   public void execute(Combatant player, Combatant target, BattleContext context) {
-    int damage=Math.max(0, actor.getAttack()-target.getDefense());
+    int damage=Math.max(0, player.getAttack()-target.getDefense());
     target.takeDamage(damage);
-    System.out.println(actor.getName()+" attacks "+target.getName()+" for "+damage+" damage.");
+    System.out.println(player.getName()+" attacks "+target.getName()+" for "+damage+" damage.");
     }
 
     public String getName(){
