29dfa7866f2b2d70aebaa90381d21fb13004c673
ThomasChai288 <thomas.chai.qin.tze@gmail.com>
2026-03-20 17:08:19 +0800

    Create UseItemAction

--- UseItemAction.java
+++ UseItemAction.java
@@ -1,12 +1,12 @@
 public class UseItemAction implements Action {
   private Item item;
-  public UseItemAction(Item item) {
+  public UseItemAction(Item item){
     this.item=item;
   }
-  public void execute(Combatant player, Combatant target, BattleContext context) {
+  public void execute(Combatant player, Combatant target, BattleContext context){
     item.use(player, context);
   }
-  public String getName() {
+  public String getName(){
     return "Use Item";
   }
 }
