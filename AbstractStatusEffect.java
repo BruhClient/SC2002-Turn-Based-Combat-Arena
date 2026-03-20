422828f70c53d1d14ee4ad9f01c23994c2e11439
ThomasChai288 <thomas.chai.qin.tze@gmail.com>
2026-03-20 17:02:36 +0800

    Abstract class AbstractStatusEffect

---
+++ AbstractStatusEffect.java
@@ -0,0 +1,14 @@
+public abstract class AbstractStatusEffect implements StatusEffect {
+  protected int remainingTurns;
+  public AbstractStatusEffect(int duration) {
+    this.remainingTurns = duration;
+  }
+  public void onApply(Combatant target) {}
+  public void onTurnStart(Combatant target) {}
+  public void onTurnEnd(Combatant target) {
+    remainingTurns--;
+  }
+  public boolean isExpired() {
+    return remainingTurns<=0;
+  }
+}
