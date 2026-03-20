03c7e6400d7a2d0167d47aa481d11f7b649cc0a3
ThomasChai288 <thomas.chai.qin.tze@gmail.com>
2026-03-20 17:11:27 +0800

    Create SmokeBombEffect

---
+++ SmokeBombEffect.java
@@ -0,0 +1,11 @@
+public class SmokeBombEffect extends AbstractStatusEffect{
+  public SmokeBombEffect(int duration){
+    super(duration);
+  }
+  public String getName(){
+    return "Smoke Bomb";
+  }
+  public boolean negateDamage(){
+    return true;
+  }
+}
