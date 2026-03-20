2457acb99912356558577ee35397a9ad88355b16
ThomasChai288 <thomas.chai.qin.tze@gmail.com>
2026-03-20 17:05:07 +0800

    Create StunEffect

---
+++ StunEffect.java
@@ -0,0 +1,11 @@
+public class StunEffect extends AbstractStatusEffect{
+  public StunEffect(int duration){
+    super(duration);
+  }
+  public void onApply(Combatant target){
+    System.out.println(target.getName()+" is stunned!");
+  }
+  public String getName(){
+    return "Stun";
+  }
+}
