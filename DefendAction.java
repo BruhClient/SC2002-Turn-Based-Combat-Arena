667de1feebee68b3c16868d07455b735a184a3f4
ThomasChai288 <thomas.chai.qin.tze@gmail.com>
2026-03-20 16:56:03 +0800

    Create DefendAction

---
+++ DefendAction.java
@@ -0,0 +1,9 @@
+public class DefendAction implements Action{
+  public void execute(Combatant player, Combatant target, BattleContext context) {
+    player.addStatusEffect(new DefendEffect(2));
+    System.out.println(player.getName()+" is defending!");
+  }
+  public String getName() {
+    return "Defend";
+  }
+}
