141d71193fe9d970179fb6ce1d7de7c56e2cb178
ThomasChai288 <thomas.chai.qin.tze@gmail.com>
2026-03-20 18:35:52 +0800

    Create BattleContext

---
+++ BattleContext.java
@@ -0,0 +1,22 @@
+public class BattleContext{
+  private List<Combatant> allCombatants;
+  public BattleContext(List<Combatant> combatants){
+    this.allCombatants=combatants;
+  }
+  public List<Combatant> getEnemiesOf(Combatant player){
+    return allCombatants.stream()
+      .filter(c->c.isEnemy()!=actor.isEnemy() && c.isAlive())
+      .toList();
+  }
+  public List<Combatant> getAlliesOf(Combatant player){
+    return allCombatants.stream()
+      .filter(c->c.isEnemy()==actor.isEnemy() && c.isAlive())
+      .toList();
+  }
+  public void addStatusEffect(Combatant target, StatusEffect effect{
+    target.addStatusEffect(effect);
+  }
+  public void removeCombatant(Combatant combatant){
+    allCombatants.remove(combatant);
+  }
+}
