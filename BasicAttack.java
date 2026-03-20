f3e2ff1b9771a67de51c0a22bb07c6b30b020370
ThomasChai288 <thomas.chai.qin.tze@gmail.com>
2026-03-20 17:06:18 +0800

    Create BasicAttack

--- BasicAttack.java
+++ BasicAttack.java
@@ -1,5 +1,5 @@
-public class BasicAttack implements Action {
-  public void execute(Combatant player, Combatant target, BattleContext context) {
+public class BasicAttack implements Action{
+  public void execute(Combatant player, Combatant target, BattleContext context){
     int damage=Math.max(0, player.getAttack()-target.getDefense());
     target.takeDamage(damage);
     System.out.println(player.getName()+" attacks "+target.getName()+" for "+damage+" damage.");
