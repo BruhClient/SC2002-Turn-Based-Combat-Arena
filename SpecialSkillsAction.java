129fc371559b47cf91011a7c2100d0ea94cb7d9a
ThomasChai288 <thomas.chai.qin.tze@gmail.com>
2026-03-20 17:07:18 +0800

    Update SpecialSkillsAction.java

--- SpecialSkillsAction.java
+++ SpecialSkillsAction.java
@@ -1,5 +1,5 @@
 public class SpecialSkillAction implements Action{
-  public void execute(Combatant player, Combatant target, BattleContext context) {
+  public void execute(Combatant player, Combatant target, BattleContext context){
     player.useSpecialSkill(context);
   }
   public String getName(){
