4ff7e3a45cd4edaa4b742f401d450280a7230806
ThomasChai288 <thomas.chai.qin.tze@gmail.com>
2026-03-20 16:58:41 +0800

    Create SpecialSkillsAction

---
+++ SpecialSkillsAction.java
@@ -0,0 +1,8 @@
+public class SpecialSkillAction implements Action{
+  public void execute(Combatant player, Combatant target, BattleContext context) {
+    player.useSpecialSkill(context);
+  }
+  public String getName(){
+    return "Special Skill";
+  }
+}
