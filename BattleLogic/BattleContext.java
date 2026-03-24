b4646ada8fee5f5b5fbd36acad733720127178a2
ThomasChai288 <thomas.chai.qin.tze@gmail.com>
2026-03-24 14:13:39 +0800

    Update BattleContext.java

--- BattleLogic/BattleContext.java
+++ BattleLogic/BattleContext.java
@@ -5,24 +5,36 @@ import java.util.List;
 import Combatants.Combatant;
 
 public class BattleContext{
-  private List<Combatant> allCombatants;
-  public BattleContext(List<Combatant> combatants){
-    this.allCombatants=combatants;
-  }
-  public List<Combatant> getEnemiesOf(Combatant player){
-    return allCombatants.stream()
-      .filter(c->c.isEnemy()!=actor.isEnemy() && c.isAlive())
-      .toList();
-  }
-  public List<Combatant> getAlliesOf(Combatant player){
-    return allCombatants.stream()
-      .filter(c->c.isEnemy()==actor.isEnemy() && c.isAlive())
-      .toList();
-  }
-  public void addStatusEffect(Combatant target, StatusEffect effect){
-    target.addStatusEffect(effect);
-  }
-  public void removeCombatant(Combatant combatant){
-    allCombatants.remove(combatant);
-  }
+    private List<Combatant> allies;
+    private List<Combatant> enemies;
+
+    public BattleContext(List<Combatant> allies, List<Combatant> enemies){
+      this.allies=allies;
+      this.enemies=enemies;
+    }
+
+    public List<Combatant> getEnemiesOf(Combatant player){
+      if (allies.contains(player)){
+        return enemies.stream().filter(Combatant::isAlive).toList();
+      } else {
+        return allies.stream().filter(Combatant::isAlive).toList();
+      }
+    }
+
+    public List<Combatant> getAlliesOf(Combatant player){
+      if (allies.contains(actor)) {
+        return allies.stream().filter(Combatant::isAlive).toList();
+      } else {
+        return enemies.stream().filter(Combatant::isAlive).toList();
+      }
+    }
+
+    public void addStatusEffect(Combatant target, StatusEffect effect){
+      target.addStatusEffect(effect);
+    }
+
+    public void removeCombatant(Combatant combatant){
+      allies.remove(combatant);
+      enemies.remove(combatant);
+    }
 }
