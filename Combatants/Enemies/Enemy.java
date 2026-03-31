4897606c74bf2385338a5c66bbee00ee8883a6ee
rongtian <rongtian444@gmail.com>
2026-03-31 14:15:34 +0800

    Update Enemy.java

--- Combatants/Enemies/Enemy.java
+++ Combatants/Enemies/Enemy.java
@@ -1,9 +1,25 @@
 package Combatants.Enemies;
 
+import Actions.BasicAttackAction;
+import BattleLogic.BattleContext;
 import Combatants.Combatant;
+import Combatants.Players.Player;
 
 public abstract class Enemy extends Combatant {
-    public Enemy(String name, int maxHp, int attack, int defense, int speed) {
+    private EnemyStrategy actionStrategy;
+    private Player target; // Since enemy can only attack the player
+
+    public Enemy(String name, int maxHp, int attack, int defense, int speed) { // constructor
         super(name, maxHp, attack, defense, speed);
+    }
+
+    public void performAction(BattleContext context) { // requires context as argument
+        BasicAttackAction basicAttack = new BasicAttackAction();
+        basicAttack.execute(this, target, context);   // attacker and target are always the same
+    }
+
+    public boolean isStunned() {
+        // ? TO DO
+        return false;
     }
 }
